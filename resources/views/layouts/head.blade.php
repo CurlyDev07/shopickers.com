0a37e3228f27cc4a6d1a15427d8327bbdaca18c7
reggie_frias <reggiefrias.taktyl@gmail.com>
2020-06-05 21:05:12 +0800

    google analytics gtag

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -18,6 +18,16 @@
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     @yield('css')
     @yield('head_js')
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-168677509-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-168677509-1');
+    </script>
 </head>
 <body>
 
