7ffb8908b97f3d04bf6837ee1e449a41a6e53da5
reggie_frias <reggiefrias.taktyl@gmail.com>
2020-06-16 22:34:45 +0800

    add Socialite to config

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Laravel\Socialite\SocialiteServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -234,6 +235,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
